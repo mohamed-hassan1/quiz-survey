dd129c02603a8b6081f34e08d69946bff2d3e737
mohamed-hassan1 <mohamedhassanmohamed1@gmail.com>
2025-11-06 02:51:04 +0200

    Quiz Slide Animation

--- Mini Basic Quiz/main.js
+++ Mini Basic Quiz/main.js
@@ -1,13 +1,21 @@
 (function() {
   'use strict';
   // The Right Answers for the 3 Questions Attempts
-  const answers = ['a', 'c', 'd'];
+  const answers = ['a', 'c', 'd'],
+        attempts = 3;
 
   const quizContainer = document.querySelector('.quiz-container'),
-        quizPopup = quizContainer.querySelector('.popup-container');
+        quizPopup = quizContainer.querySelector('.popup-container'),
+        quizSlides = quizContainer.querySelector('.slides'),
+        quizSlidesMargin = 14;
 
   let globalObj = {
-    attempt: 0,
+    // Counters
+    quizCounter: 0,
+    attemptCounter: 0,
+    oldSlideSpace: 0,
+
+    // Add Right Symbol
     rightSymbol: (container) => {
       if (!container.classList.contains('symbol-added')) {
         let rightEle = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="14" height="18" x="0" y="0" viewBox="0 0 511.985 511.985" xml:space="preserve" class=""><path d="M500.088 83.681c-15.841-15.862-41.564-15.852-57.426 0L184.205 342.148 69.332 227.276c-15.862-15.862-41.574-15.862-57.436 0-15.862 15.862-15.862 41.574 0 57.436l143.585 143.585c7.926 7.926 18.319 11.899 28.713 11.899 10.394 0 20.797-3.963 28.723-11.899l287.171-287.181c15.862-15.851 15.862-41.574 0-57.435z" fill="currentColor"/></svg>`,
@@ -19,6 +27,8 @@
         container.classList.add('symbol-added');
       }
     },
+
+    // Add Wrong Symbol
     wrongSymbol: (container) => {
       if (!container.classList.contains('symbol-added')) {
         let wrongEle = `<div></div>`,
@@ -29,8 +39,28 @@
         container.appendChild(div);
         container.classList.add('symbol-added');
       }
+    },
+
+    // Quiz Slide Move Animation
+    quizMoveAnim: () => {
+      if (document.head.querySelector('#quizanim')) {
+        quizSlides.style.transform = `translateX(-${globalObj.oldSlideSpace}px)`;
+        document.head.querySelector('#quizanim').remove();
+      }
+      let anim = document.createElement('style'),
+          slideSpace = (quizSlides.offsetWidth + quizSlidesMargin) * globalObj.quizCounter;
+      globalObj.oldSlideSpace = slideSpace;
+      anim.id = 'quizanim';
+      anim.innerHTML = `.${quizContainer.className} .${quizSlides.className} {transform:translateX(-${slideSpace}px)}`;
+      setTimeout(() => {
+        quizSlides.removeAttribute('style');
+        document.head.appendChild(anim)
+      },125);
     }
+
   }
+
+
 
   // Quiz Global Click
   quizContainer.addEventListener('click', function(e) {
@@ -39,14 +69,16 @@
       // Get Answer
       let answer = answerBtn.querySelector('.answer-num').getAttribute('data-answernum');
       // Check for right answer
-      if ((answers[globalObj.attempt].toLowerCase() === answer.trim().toLowerCase()) && (!answerBtn.classList.contains('ready') && !answerBtn.classList.contains('correct'))) { // Right
+      if ((answers[globalObj.attemptCounter].toLowerCase() === answer.trim().toLowerCase()) && (!answerBtn.classList.contains('ready') && !answerBtn.classList.contains('correct'))) { // Right
         answerBtn.classList.add('ready');
         globalObj.rightSymbol(answerBtn.children[0]);
         setTimeout(() => {
           answerBtn.classList.add('correct');
           answerBtn.classList.remove('ready');
+          globalObj.quizCounter += 1;
+          globalObj.quizMoveAnim();
         }, 500);
-      } else if ((answers[globalObj.attempt].toLowerCase() !== answer.trim().toLowerCase()) && (!answerBtn.classList.contains('ready') && !answerBtn.classList.contains('wrong'))) { // Wrong
+      } else if ((answers[globalObj.attemptCounter].toLowerCase() !== answer.trim().toLowerCase()) && (!answerBtn.classList.contains('ready') && !answerBtn.classList.contains('wrong'))) { // Wrong
         answerBtn.classList.add('ready');
         globalObj.wrongSymbol(answerBtn.children[0]);
         setTimeout(() => {
